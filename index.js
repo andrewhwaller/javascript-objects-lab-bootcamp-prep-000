51b65894e7c3beac769bd3d95d81c5bc1059099b
Andrew Waller <andrewhwaller@gmail.com>
2019-03-10 03:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(recipes, prop, "1") {
+function updateObjectWithKeyAndValue(recipes, prop, '1') {
   var prop2 = "2"
   return Object.assign({}, recipes, { [prop]: 1 })
   recipes.prop2 = '2'
